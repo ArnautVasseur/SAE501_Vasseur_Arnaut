a2ec9bbba6fe36bdd680c26f5e4ea9c96fa5ca97
ArnautVasseur <vasseur.arnaut@gmail.com>
2024-01-08 15:51:26 +0100

    add watch in threejs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,12 @@
 <script setup>
-
+  import Watch from "../components/Watch.vue"
 </script>
 
 <template>
-
+  <div>
+    <div></div>
+    <div class="my-2 mx-2">
+      <Watch/>
+    </div>
+  </div>
 </template>
-
-
-
-<style>
-
-</style>
